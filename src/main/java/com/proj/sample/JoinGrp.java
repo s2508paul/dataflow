1bbd084e93e05c4759fb99ca379a12eaabb9a86e
Sumit Paul <saurabh.padhy@kohls.com>
2019-04-21 01:39:05 +0530

    kk

---
+++ src/main/java/com/proj/sample/JoinGrp.java
@@ -0,0 +1,78 @@
+package com.proj.sample;
+
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.Row;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.schemas.Schema;
+import java.util.List;
+import org.apache.beam.sdk.extensions.sql.SqlTransform;
+import java.util.stream.Collectors;
+
+
+
+public class JoinGrp {
+
+
+    public static final Schema SCHEMA = Schema.builder()
+            .addInt64Field("id")
+            .addStringField("name")
+            .addDoubleField("marks")
+            .build();
+
+    public static class RowToString extends DoFn<Row, String>{
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            String line = c.element().getValues()
+                    .stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining(","));
+
+
+         /*List l = c.element().getValues();
+            System.out.println("size="+l.size());
+            */
+            c.output(line);
+        }
+
+    }
+
+    public static class StringToRow extends DoFn<String, Row> {
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            String[] vals = c.element().split(",");
+            Row appRow = Row
+                    .withSchema(SCHEMA)
+                    .addValues(Long.valueOf(vals[0]),vals[1],Double.valueOf(vals[2]))
+                    .build();
+            c.output(appRow);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().create();
+        Pipeline p = Pipeline.create(options);
+
+        String input = "/Users/tkmacgf/Desktop/to_tar/dir1/a2.txt";
+        //String outputPrefix = "/Users/tkmacgf/Desktop/to_tar/output/hello";
+        PCollection<String> lines = p.apply("readRow", TextIO.read().from(input));
+        PCollection<Row> rw = lines.apply("transform_to_row", ParDo.of(new StringToRow())).setRowSchema(SCHEMA);
+
+        PCollection<Row> new_rw = rw.apply("transform_sql", SqlTransform.query(
+                "SELECT id,name FROM PCOLLECTION where id=2 or id=3"));
+
+        PCollection<String> z = new_rw.apply("transform_to_string", ParDo.of(new RowToString()));
+
+        z.apply("write_to_gcs", TextIO.write().to("/Users/tkmacgf/Desktop/to_tar/hello").withSuffix(".txt").withoutSharding());
+        p.run();
+
+
+
+    }
+}
---
+++ src/main/java/com/proj/sample/MyFirstDf.java
@@ -0,0 +1,39 @@
+package com.proj.sample;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+
+
+public class MyFirstDf {
+
+    public static void main(String[] args) {
+
+        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().create();
+        Pipeline p = Pipeline.create(options);
+
+        String input = "/Users/tkmacgf/Desktop/to_tar/dir1/a1.txt";
+        String outputPrefix = "/Users/tkmacgf/Desktop/to_tar/output/hello";
+        final String searchTerm = "hello";
+
+
+        PCollection<String> lines = p.apply("GetJava", TextIO.read().from(input)); //1st arg is the name of transform,2nd op
+        PCollection<String> filtered_lines = lines.apply("Grep", ParDo.of(new DoFn<String, String>() {
+            @ProcessElement
+            public void processElement(ProcessContext c) throws Exception {
+                String line = c.element();
+                if (line.contains(searchTerm)) {
+                    c.output(line);
+                }
+            }
+        }));
+        filtered_lines.apply(TextIO.write().to(outputPrefix).withSuffix(".txt").withoutSharding());
+
+        p.run();
+    }
+
+}
